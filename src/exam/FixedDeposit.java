195a7b36e44bfe1de4ae461df9f2875f2a57a02c
Alver-F <rbca22108.rcca@gmail.com>
2024-02-05 19:55:18 +0530

    Commit

---
+++ src/exam/FixedDeposit.java
@@ -0,0 +1,14 @@
+package exam;
+
+public class FixedDeposit {
+	float amount;
+	float rate;
+	int period;
+	
+	FixedDeposit(float amount,float rate,int period)
+	{
+		this.amount=amount;
+		this.rate=rate;
+		this.period=period;
+	}
+}
